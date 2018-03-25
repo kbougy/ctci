55db89b7914709e55b3a88bb12add42ed55a4858
kbougy <kyriayxvanderpoel@gmail.com>
2018-03-25 03:49:52 -0500

    Add runtime notes

--- src/DataStructures/Tree.ts
+++ src/DataStructures/Tree.ts
@@ -1,4 +1,4 @@
-import LinkedList from "./LinkedList";
+import DoublyLinkedList from "./DoublyLinkedList";
 
 export default class Tree<T> {
     /**
@@ -6,19 +6,21 @@ export default class Tree<T> {
      */
 
     data: T | null;
-    children: LinkedList<Tree<T>>;
+    children: DoublyLinkedList<Tree<T>>;
 
     /**
      * Create the empty (null) tree.
      */
     constructor(data: T = null) {
         this.data = data;
-        this.children = new LinkedList<Tree<T>>();
+        this.children = new DoublyLinkedList<Tree<T>>();
     }
 
     /**
      * Insert a value into the tree.
      *
+     * @runtime_notes Using an open doubly linked list offers O(1)
+     *                back insert time.
      * @runtime O(1)
      */
     public insert(data: T) {
